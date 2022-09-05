690d53fce3097e8835ca738e750b2ccc75993b03
Worawat Yao <worawatyao@gmail.com>
2022-09-05 22:59:19 +0700

    Fix submit followup report missing property

--- lib/services/api/report_api.dart
+++ lib/services/api/report_api.dart
@@ -293,6 +293,7 @@ class ReportApi extends GraphQlBaseApi {
             id
             rendererData
             testFlag
+            createdAt
             reportType {
               id
               name
