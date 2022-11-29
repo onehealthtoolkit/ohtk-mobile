ef99ddae4b8fbdd8eb7f944984587f285621587a
Worawat Yao <worawatyao@gmail.com>
2022-11-29 14:05:20 +0700

    Update home layout to support bottom navigation bar

--- lib/ui/home/home_view_model.dart
+++ lib/ui/home/home_view_model.dart
@@ -1,34 +1,19 @@
 import 'package:podd_app/locator.dart';
-import 'package:podd_app/models/entities/report.dart';
 import 'package:podd_app/models/user_profile.dart';
 import 'package:podd_app/services/auth_service.dart';
-import 'package:podd_app/services/config_service.dart';
 import 'package:podd_app/services/notification_service.dart';
-import 'package:podd_app/services/report_service.dart';
 import 'package:stacked/stacked.dart';
 
 class HomeViewModel extends IndexTrackingViewModel {
   final INotificationService notificationService =
       locator<INotificationService>();
   IAuthService authService = locator<IAuthService>();
-  IReportService reportService = locator<IReportService>();
-  ConfigService configService = locator<ConfigService>();
+
   UserProfile? get userProfile => authService.userProfile;
-
-  int get numberOfReportPendingToSubmit => reportService.pendingReports.length;
-
-  List<Report> get pendingReports => reportService.pendingReports;
-
-  logout() {
-    authService.logout();
-  }
 
   String? get username => userProfile?.username;
 
   bool get isConsent => userProfile?.consent ?? false;
-
-  @override
-  List<ReactiveServiceMixin> get reactiveServices => [reportService];
 
   setupFirebaseMessaging({
     NotificationMessageCallback? onBackgroundMessage,
---
+++ lib/ui/home/report_home_view.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:podd_app/ui/home/all_reports_view.dart';
+import 'package:podd_app/ui/home/my_reports_view.dart';
+import 'package:podd_app/ui/home/report_home_view_model.dart';
+import 'package:podd_app/ui/report_type/report_type_view.dart';
+import 'package:podd_app/ui/resubmit/resubmit_view.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_hooks/stacked_hooks.dart';
+
+class ReportHomeView extends HookWidget {
+  const ReportHomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TabController _tabController = useTabController(initialLength: 2);
+    final primaryColor = Theme.of(context).primaryColor;
+    TabBar _tabBar = TabBar(
+      controller: _tabController,
+      labelColor: primaryColor,
+      labelStyle: TextStyle(color: primaryColor),
+      indicator: UnderlineTabIndicator(
+        borderSide: BorderSide(color: primaryColor, width: 2),
+      ),
+      tabs: [
+        Tab(
+          child: Text(AppLocalizations.of(context)!.allReportTabLabel),
+        ),
+        Tab(
+          child: Text(AppLocalizations.of(context)!.myReportTabLabel),
+        ),
+      ],
+    );
+
+    return ViewModelBuilder<ReportHomeViewModel>.nonReactive(
+      viewModelBuilder: () => ReportHomeViewModel(),
+      builder: (context, viewModel, child) => Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: ColoredBox(
+            color: Colors.white,
+            child: _tabBar,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ReportTypeView(),
+              ),
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _ReSubmitBlock(),
+              Expanded(
+                child: TabBarView(controller: _tabController, children: [
+                  AllReportsView(),
+                  MyReportsView(),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ReSubmitBlock extends HookViewModelWidget<ReportHomeViewModel> {
+  @override
+  Widget buildViewModelWidget(
+      BuildContext context, ReportHomeViewModel viewModel) {
+    if (viewModel.numberOfReportPendingToSubmit > 0) {
+      return TextButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const ReSubmitView(),
+            ),
+          );
+        },
+        child: Text(
+            "${viewModel.numberOfReportPendingToSubmit} reports still pending to submit tap here to re-submit"),
+      );
+    }
+    return Container();
+  }
+}
