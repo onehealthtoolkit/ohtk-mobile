a850b4877b064b812dc2f0319c8956262e4b04c0
pphetra <pphetra@gmail.com>
2023-06-20 18:09:05 +0700

    hot fix: show/hide keyboard cause app to rerender.

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,10 @@ void main() async {
   setupRemoteConfig(environment);
   setupLocator(environment);
   setupTheme();
-  await ScreenUtil.ensureScreenSize();
+
   runApp(
     const RestartWidget(
-      child: MyApp(),
+      child: WrapperApp(),
     ),
   );
 }
@@ -54,22 +54,6 @@ Future<Locale> fetchLocaleFromPreference() async {
   return Locale(languageCode, '');
 }
 
-enum DeviceType { Phone, Tablet }
-
-DeviceType getDeviceType() {
-  final data = MediaQueryData.fromWindow(WidgetsBinding.instance.window);
-  return data.size.shortestSide < 550 ? DeviceType.Phone : DeviceType.Tablet;
-}
-
-Size getScreenSize() {
-  switch (getDeviceType()) {
-    case DeviceType.Phone:
-      return const Size(360, 640);
-    case DeviceType.Tablet:
-      return const Size(768, 1024);
-  }
-}
-
 setupRemoteConfig(String environment) async {
   final remoteConfig = FirebaseRemoteConfig.instance;
   if (environment == 'dev') {
@@ -79,6 +63,42 @@ setupRemoteConfig(String environment) async {
     ));
   } else {
     // do nothing use default remote config settings
+  }
+}
+
+/*
+ * hotfix: @todo fix this later
+ * ScreenUtil is a Flutter library for adapting screen and font size.
+ * but when upgrade to flutter 3.10.5 with dart version3,  
+ * there are a problem that cause the app to rebuild when keyboard show/hide
+ * so we need to make if not rebuild when keyboard show/hide by wrapping with builder that return the same widget
+ */
+class WrapperApp extends StatelessWidget {
+  const WrapperApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      var mediaQueryData = MediaQuery.of(context);
+      ScreenUtil.configure(
+        data: mediaQueryData,
+        designSize: getScreenSize(mediaQueryData),
+        minTextAdapt: true,
+        splitScreenMode: true,
+      );
+
+      return const MyApp();
+    });
+  }
+
+  Size getScreenSize(MediaQueryData data) {
+    if (data.size.shortestSide < 550) {
+      // Phone
+      return const Size(360, 640);
+    } else {
+      // Tablet
+      return const Size(768, 1024);
+    }
   }
 }
 
@@ -166,7 +186,7 @@ class RestartWidget extends StatefulWidget {
   }
 
   @override
-  _RestartWidgetState createState() => _RestartWidgetState();
+  createState() => _RestartWidgetState();
 }
 
 class _RestartWidgetState extends State<RestartWidget> {
