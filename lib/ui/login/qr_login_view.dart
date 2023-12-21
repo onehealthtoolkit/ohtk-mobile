f9dd60cabd4948263398b05d21456e251b8537e7
Worawat Yao <worawatyao@gmail.com>
2023-12-21 17:21:59 +0700

    Improve qr scanner screen with transparent viewfinder

--- lib/ui/login/qr_login_view.dart
+++ lib/ui/login/qr_login_view.dart
@@ -55,38 +55,42 @@ class QrLoginView extends StatelessWidget {
               },
             ),
             _framer(context),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: FlatButton.primary(
-                onPressed: () async {
-                  final image = await _pickImage(ImageSource.gallery);
-                  if (image != null) {
-                    String? data = await Scan.parse(image.path);
-                    String? error;
-                    if (data != null) {
-                      error = await viewModel.authenticate(data);
-                    } else {
-                      error = 'Failed to read QR Code';
-                    }
-                    if (context.mounted) {
-                      GoRouter.of(context).pop(error);
-                    }
-                  }
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.qr_code,
-                      size: 24,
-                    ),
-                    const SizedBox(width: 4),
-                    Text(AppLocalizations.of(context)!.pickQrcodeImageButton),
-                  ],
-                ),
-              ),
-            ),
+            _qrPicker(viewModel, context),
             if (viewModel.isBusy) _waitingProgress(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _qrPicker(QrLoginViewModel viewModel, BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: FlatButton.primary(
+        onPressed: () async {
+          final image = await _pickImage(ImageSource.gallery);
+          if (image != null) {
+            String? data = await Scan.parse(image.path);
+            String? error;
+            if (data != null) {
+              error = await viewModel.authenticate(data);
+            } else {
+              error = 'Failed to read QR Code';
+            }
+            if (context.mounted) {
+              GoRouter.of(context).pop(error);
+            }
+          }
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.qr_code,
+              size: 24,
+            ),
+            const SizedBox(width: 4),
+            Text(AppLocalizations.of(context)!.pickQrcodeImageButton),
           ],
         ),
       ),
@@ -105,7 +109,11 @@ class QrLoginView extends StatelessWidget {
         child: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            OhtkProgressIndicator(size: 80),
+            CircleAvatar(
+              backgroundColor: Colors.black54,
+              radius: 40,
+              child: OhtkProgressIndicator(size: 80),
+            ),
             SizedBox(height: 16),
             Text(
               "Please wait...",
@@ -120,21 +128,109 @@ class QrLoginView extends StatelessWidget {
     );
   }
 
-  Center _framer(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width / 1.5,
-        height: 250,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            border: Border.all(
-              color: Colors.blue.shade300,
-              width: 3,
+  Stack _framer(BuildContext context) {
+    double scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 200.0
+        : 330.0;
+
+    return Stack(children: [
+      ColorFiltered(
+        colorFilter: const ColorFilter.mode(
+            Colors.black38, BlendMode.srcOut), // This one will create the magic
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  color: Colors.black,
+                  backgroundBlendMode: BlendMode
+                      .dstOut), // This one will handle background + difference out
             ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: scanArea,
+                width: scanArea,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: CustomPaint(
+          foregroundPainter: BorderPainter(),
+          child: SizedBox(
+            width: scanArea + 25,
+            height: scanArea + 25,
           ),
         ),
       ),
+    ]);
+  }
+}
+
+// Creates the white borders
+class BorderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    const width = 4.0;
+    const radius = 20.0;
+    const tRadius = 3 * radius;
+    final rect = Rect.fromLTWH(
+      width,
+      width,
+      size.width - 2 * width,
+      size.height - 2 * width,
     );
+    final rrect = RRect.fromRectAndRadius(rect, const Radius.circular(radius));
+    const clippingRect0 = Rect.fromLTWH(
+      0,
+      0,
+      tRadius,
+      tRadius,
+    );
+    final clippingRect1 = Rect.fromLTWH(
+      size.width - tRadius,
+      0,
+      tRadius,
+      tRadius,
+    );
+    final clippingRect2 = Rect.fromLTWH(
+      0,
+      size.height - tRadius,
+      tRadius,
+      tRadius,
+    );
+    final clippingRect3 = Rect.fromLTWH(
+      size.width - tRadius,
+      size.height - tRadius,
+      tRadius,
+      tRadius,
+    );
+
+    final path = Path()
+      ..addRect(clippingRect0)
+      ..addRect(clippingRect1)
+      ..addRect(clippingRect2)
+      ..addRect(clippingRect3);
+
+    canvas.clipPath(path);
+    canvas.drawRRect(
+      rrect,
+      Paint()
+        ..color = Colors.white
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = width,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
